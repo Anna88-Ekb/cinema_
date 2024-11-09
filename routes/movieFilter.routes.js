3a6d8635610c071349d01968bbc8cf9450304090
Anna88-Ekb <annas.web.ekb@gmail.com>
2024-11-10 01:05:10 +0300

    0911

--- routes/movieFilter.routes.js
+++ routes/movieFilter.routes.js
@@ -10,5 +10,9 @@ router.get('/movie-months', movieFilterSelection.getMoviesMonthByMovieName);
 router.get('/movie-day-calendar', movieFilterSelection.getDaysByMovieNameAndMonth);
 router.get('/movie-time-calendar', movieFilterSelection.getTimeByMovieNameAndDay);
 router.get('/movie-hall-calendar', movieFilterSelection.getHallByMovieNameAndDatetime);
+router.get('/movie-all-country', movieFilterSelection.getALLMovieCountry);
+router.get('/movie-all-type', movieFilterSelection.getALLMovieType);
+router.get('/movie-all-age', movieFilterSelection.getALLMovieAge);
+router.get('/movie-all-hall', movieFilterSelection.getALLHall);
 
 export default router;
